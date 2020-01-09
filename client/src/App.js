c45defffa0a367bedae333eb6d7034ac2bd79426
Ashley Clarke <crashley92@gmail.com>
2020-01-08 18:13:21 -0800

    trying again

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from './component/NavTabs/NavTabs';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import UserProfile from './pages/UserProfile';
 
-function App() {
-  return (
-    <Router>
-    <NavTabs />
-    <Route exact path="/" component={Home} />
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <NavTabs />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/signup" component={Signup} />
+      <Route exact path="/userprofile" component={UserProfile} />
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ client/src/pages/Signup.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class Signup extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: ''
+    }
+
+    postDataHandler = () => {
+        const data = {
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        axios.post('http://localhost:3001/api/signup', data)
+        .then(response => {
+            console.log(response);
+        }).catch((err) => {
+            console.log(err);
+        }) 
+    }
+
+    render() {
+        return (
+             <div className="signup-page">
+                <h1>Sign up</h1>
+                <label>Username</label>
+                <input type="text" value={this.state.username} onChange={(event) => this.setState({username: event.target.value})} />
+                <label>Email</label>
+                <input type="text" value={this.state.email} onChange={(event) => this.setState({email: event.target.value})} />
+                <label>Password</label>
+                <input type="password" value={this.state.password} onChange={(event) => this.setState({password: event.target.value})} />
+                <button onClick={this.postDataHandler}>Submit</button>
+            </div>
+        )
+    }
+}
+
+export default Signup;

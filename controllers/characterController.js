c4c6df552796499eab8795ac495da95dff385339
Cedar <alexianfireflies@gmail.com>
2020-01-08 21:26:21 -0800

    routes maybe

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -1,31 +1,29 @@
 const db = require("../models/Character");
 
-var email;
-
 // Defining methods for the controller
 module.exports = {
     findAll: function(req, res) {
-        db.Character.find({email: req.body.email})
+        db.Character.find({email: req.params.email})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     findOne: function(req, res) {
-        db.Character.findOne({email: req.body.email}, {name: req.body.name})
+        db.Character.findOne({email: req.params.email, name: req.body.name})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Character.create({email: req.body.email}, req.body)
+        db.Character.create({email: req.params.email}, req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
-        db.Character.findOneAndUpdate({email: req.body.email}, req.body)
+        db.Character.findOneAndUpdate({email: req.params.email, name: req.body.name}, req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-        db.Character.findOne({email: req.body.email})
+        db.Character.findOne({email: req.params.email, name: req.body.name})
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));

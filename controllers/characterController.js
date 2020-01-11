758b18838447c37f27bed4faa3cddef321c1d5eb
Cedar <alexianfireflies@gmail.com>
2020-01-11 10:39:02 -0800

    changes to make character model use userID

--- controllers/characterController.js
+++ controllers/characterController.js
@@ -3,27 +3,27 @@ const db = require("../models/Character");
 // Defining methods for the controller
 module.exports = {
     findAll: function(req, res) {
-        db.Character.find({email: req.params.email})
+        db.Character.find({userID: req.params.userID})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     findOne: function(req, res) {
-        db.Character.findOne({email: req.params.email, name: req.body.name})
+        db.Character.findOne({userID: req.params.userID, charID: req.params.charID})
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     create: function(req, res) {
-        db.Character.create({email: req.params.email}, req.body)
+        db.Character.create({userID: req.params.userID}, req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     update: function(req, res) {
-        db.Character.findOneAndUpdate({email: req.params.email, name: req.body.name}, req.body)
+        db.Character.findOneAndUpdate({userID: req.params.userID, charID: req.params.charID}, req.body)
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
     },
     remove: function(req, res) {
-        db.Character.findOne({email: req.params.email, name: req.body.name})
+        db.Character.findOne({userID: req.params.userID, charID: req.params.charID})
         .then(dbModel => dbModel.remove())
         .then(dbModel => res.json(dbModel))
         .catch(err => res.status(422).json(err));
--- models/Character.js
+++ models/Character.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var CharacterSchema = new Schema({
-    email: {
+    userID: {
         type: String,
         required: true
     },

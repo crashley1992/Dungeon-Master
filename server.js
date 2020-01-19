416c84930b9eeedeff8ca48c25cf492540f2a469
Ashley Clarke <crashley92@gmail.com>
2020-01-19 14:13:03 -0800

    reconfigured server file

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ if(!isProduction) {
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 //Configure Mongoose for Passwords/Logins
